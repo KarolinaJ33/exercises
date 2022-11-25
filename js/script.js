9939df6d856d36e3bd0457afa6944fabbd57b395
Karolina Jasiówka <jasiowkakarolina@gmail.com>
2022-11-25 12:43:54 +0100

    corrected line

--- js/script.js
+++ js/script.js
@@ -36,6 +36,7 @@
                 removeTask(index);
             });
         });
+        
         const toggleDoneButtons = document.querySelectorAll(".js-done");
 
         toggleDoneButtons.forEach((toggleDoneButton, index) => {
